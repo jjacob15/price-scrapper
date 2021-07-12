cf7b6afce99d22e2e893b65afbc406b2248ee0a7
jjacob15 <jaison.jack@gmail.com>
2021-07-12 08:58:29 +0800

    update

--- getpricewithasync.py
+++ getpricewithasync.py
@@ -11,24 +11,22 @@ class HtmlSearch():
             result = self._driver.find_element_by_id(id_=tagId)
             return result.text
         except Exception as ex:
-            raise Exception(
-                "Unable to find price from the url. Please check the Url. " + str(ex))
+            return "No price avaiable"
 
     def findbyclass(self, className):
         try:
             result = self._driver.find_element_by_class_name(className)
             return result.text
         except Exception as ex:
-            raise Exception(
-                "Unable to find price from the url. Please check the Url. " + str(ex))
+            return "No price avaiable"
 
 
 if __name__ == "__main__":
     path = "C:\\Users\\jaison.jacob\\Desktop\\scrapped-price.txt"
 
-    bbqUrl = "https://www.appliancesonline.com.au/product/beefeater-bb722aa-bigg-bugg-amber-mobile-bbq"
-    B = HtmlSearch(bbqUrl)
-    bbqprice = B.findbyclass("aol-product-price")
+    bear = "https://www.amazon.com.au/Very-Cranky-Bear-Book-Boxed/dp/1760971316/ref=sr_1_1?crid=K1Y5CYT162Z1&dchild=1&keywords=the+very+cranky+bear&qid=1626051214&sprefix=the+very+cran%2Caps%2C403&sr=8-1"
+    B = HtmlSearch(bear)
+    bbqprice = B.findbyid("price")
 
     spoonUrl = "https://www.royaldoultonoutlet.com.au/vera-wang-wedgwood-chime-nouveau-16-piece-cutlery-set.html"
     H = HtmlSearch(spoonUrl)
@@ -38,7 +36,7 @@ if __name__ == "__main__":
         with open(path, "a") as filehandle:
             filehandle.write('Price of spoons on ' + str(datetime.datetime.today().strftime(
                 "%b %d %Y %H:%M:%S")) + ' is ' + spoonprice + '\n')
-            filehandle.write('Price of bbq on ' + str(datetime.datetime.today().strftime(
+            filehandle.write('Price of bear on ' + str(datetime.datetime.today().strftime(
                 "%b %d %Y %H:%M:%S")) + ' is ' + bbqprice + '\n')
     except Exception as ex:
         raise Exception("Unable to write the result on file. " + str(ex))    
